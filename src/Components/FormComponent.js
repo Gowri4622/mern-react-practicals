e5fc0fb20e93aa9f94ea0ae98cc0c007fd484d4c
Gowri4622 <gowrihifi@gmail.com>
2023-06-12 14:32:13 +0530

    Initial commit.

---
+++ src/Components/FormComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class FormComponent extends Component {
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default FormComponent
